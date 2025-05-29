4b9b6213848c746f208fe1209a3b71dcf0850e58
asafelobotomy <coeliac.ranking-2s@icloud.com>
2025-05-29 11:56:50 +0100

    Add files via upload

--- xanados-iso/profiledef.sh
+++ xanados-iso/profiledef.sh
@@ -10,18 +10,25 @@ install_dir="xanados"
 buildmodes=("iso")
 bootmodes=("uefi-x64.systemd-boot.esp")
 
-# File system
+# File system and compression
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=()
 airootfs_image_tool_options+=("-comp" "zstd" "-Xcompression-level" "19")
 file_permissions=()
 
-# Optional: copy files with root:root
+# Permissions example
 file_permissions+=(["/etc/shadow"]="0:0:400")
 
-# Default ISO boot background
-# You can later set GRUB and Plymouth themes in /airootfs
+# Paths - adjust this to your build directory if different
+iso_prep="$(pwd)/work/x86_64"
 
-# Custom hooks (none yet)
-# file_permissions+=([“/usr/bin/xanados-hook”]=“0:0:755”)
+# Ensure EFI bootloader is installed properly
+install_efi_bootloader() {
+    mkdir -p "${iso_prep}/efiboot/EFI/BOOT"
+    bootctl --path="${iso_prep}/efiboot" install
+    cp /usr/lib/systemd/boot/efi/systemd-bootx64.efi "${iso_prep}/efiboot/EFI/BOOT/BOOTX64.EFI"
+}
+
+# Call the EFI bootloader installer at the end of ISO prep
+install_efi_bootloader
