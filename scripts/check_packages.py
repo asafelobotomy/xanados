b3fc6dc3b51fca0bf0f6394f75b8c9a74bb95e85
aSafeLobotomy <coeliac.ranking-2s@icloud.com>
2025-06-05 18:40:18 +0100

    Update check_packages.py

--- scripts/check_packages.py
+++ scripts/check_packages.py
@@ -8,9 +8,6 @@ warnings.filterwarnings("ignore", category=InsecureRequestWarning)
 import json
 import sys
 from pathlib import Path
-import json
-import sys
-from pathlib import Path
 
 packages_file = Path("xanados-iso/packages.x86_64")
 if not packages_file.exists():
